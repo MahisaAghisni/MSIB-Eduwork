c0d055a2e34e38ecd7e88a4a418915584a7a0058
MahisaAghisni <aghisnifadhli@gmail.com>
2023-09-23 17:35:31 +0700

    add tugas bootstrap

---
+++ Bootstrap/Tugas/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#typed', {
+    strings: ['"Sebelumnya saya tidak punya background IT karena jurusan Kimia, setelah saya belajar coding di Eduwork, Saya berhasil diterima kerja di Tokopedia sebagai Programmer."'],
+    typeSpeed: 50,
+    startDelay: 100,
+    loop: true,
+});
