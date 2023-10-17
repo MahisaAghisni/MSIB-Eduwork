7f0afb120ebc42471d056a1b7920658494ec186d
MahisaAghisni <aghisnifadhli@gmail.com>
2023-10-17 21:09:39 +0700

    edit number 2

--- PHP/tugas_php/sesi25.php
+++ PHP/tugas_php/sesi25.php
@@ -14,7 +14,7 @@ echo "<br>";
 
 $tahun = 2023;
 
-if ($tahun % 4 == 0) {
+if (($tahun % 4 == 0 && $tahun % 100 != 0) || $tahun % 400 == 0) {
     echo "$tahun adalah tahun kabisat.";
 } else {
     echo "$tahun bukan tahun kabisat.";
